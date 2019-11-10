507f440839e6aabd52378fed79e2d8ca020bcb00
kurlov-v <bazilevsvvv@gmail.com>
2019-11-10 14:18:31 +0300

    added inputs generating

--- expert-list.js
+++ expert-list.js
@@ -1,4 +1,24 @@
 function addInputsForLevels() {
-    let count = document.getElementById('levls-count').value;
+    let count = document.getElementById('levels-count').value;
+    let featureName = document.getElementById('feature-name').value
+    let featureWeight = document.getElementById('feature-weight').value
+
+    let container = document.getElementById('level-inputs');
+    let feature = document.createElement("div");
+
+    let featureNameElement = document.createElement("h3");
+    featureNameElement.appendChild(document.createTextNode(featureName));
+    let featureWeightElement = document.createElement("h4");
+    featureWeightElement.appendChild(document.createTextNode("Вес: " + featureWeight));
+    feature.appendChild(featureNameElement);
+    feature.appendChild(featureWeightElement);
+    // console.log(featureNameElement);
+
+    for (i = 0; i < count; i++) {
+        let levelInput = document.createElement("input");
+        levelInput.setAttribute("id", "levelInput" + i);
+        feature.appendChild(levelInput);     
+    }
+    container.appendChild(feature);     
 
 }
