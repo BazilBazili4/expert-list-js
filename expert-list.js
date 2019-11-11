585f8c34e4a2f40923a5a4b78da2c16522ec2ccb
kurlov-v <bazilevsvvv@gmail.com>
2019-11-11 14:30:31 +0300

    added functions for features

--- expert-list.js
+++ expert-list.js
@@ -1,7 +1,12 @@
+let featureCount = 1;
+let features = [];
+
 function addInputsForLevels() {
     let count = document.getElementById('levels-count').value;
     let featureName = document.getElementById('feature-name').value
     let featureWeight = document.getElementById('feature-weight').value
+    let featureType = document.getElementById('feature-type').value
+    // let featureType = document.getElementById('feature-type').value
 
     let container = document.getElementById('level-inputs');
     let feature = document.createElement("div");
@@ -10,18 +15,22 @@ function addInputsForLevels() {
     featureNameElement.appendChild(document.createTextNode(featureName));
     let featureWeightElement = document.createElement("h4");
     featureWeightElement.appendChild(document.createTextNode("Вес: " + featureWeight));
+
     feature.appendChild(featureNameElement);
     feature.appendChild(featureWeightElement);
-    // console.log(featureNameElement);
+
 
     for (i = 0; i < count; i++) {
         let levelInput = document.createElement("input");
-        levelInput.setAttribute("id", "featureName" + i);
+        levelInput.setAttribute("id", featureCount + "featureName" + i);
         levelInput.setAttribute("class", "levelInput");
-
-        feature.appendChild(levelInput);     
+        levelInput.required = true;
+        feature.appendChild(levelInput);   
     }
-    console.log(document.querySelectorAll( ".levelInput" ));
+    features.push(createFeature(featureCount, featureName, featureWeight, featureType));
+    featureCount += 1;
+
+    console.log(features);
     container.appendChild(feature);     
 }
 
@@ -33,4 +42,28 @@ function hideListCreation() {
 function showListUsing() {
     let listUsingDiv = document.getElementById('list-using');
     listUsingDiv.setAttribute("style", "display: block");
+}
+
+function createFeature(featureId, featureName, featureWeight, featureType) {
+    let feature = {
+        id: featureId,
+        name: featureName,
+        weight: featureWeight,
+        type: featureType,
+        levels: [],
+        value: 0
+    };
+    return feature;
+}
+
+function normalizingCoef(weightSum) {
+    return 100 / weightSum;
+}
+
+function featureRank(normalizingCoef, featureWeight) {
+    return normalizingCoef * featureWeight;
+}
+
+function levelUnit(featureRank, levelsCount) {
+    return featureRank / (featureCount - 1);
 }
